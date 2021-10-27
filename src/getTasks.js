44405c2a80262a37e8c68fcda47d704754b3aeae
micheaol <micheaol80@gmail.com>
2021-10-27 23:25:40 +0100

    fix: add if statement to the printTask method

--- src/getTasks.js
+++ src/getTasks.js
@@ -19,46 +19,48 @@ export function addtoLocal() {
 function printTasks() {
   addtoLocal();
   Object.keys(localStorage).forEach((key) => {
-    const data = JSON.parse(localStorage.getItem(key));
-    if (data) {
-      const li = createMyElement('li');
-      li.innerHTML = data.description;
-      const ul = getMyElement('#inner-tasks');
+    if (key) {
+      const data = JSON.parse(localStorage.getItem(key));
+      if (data) {
+        const li = createMyElement('li');
+        li.innerHTML = data.description;
+        const ul = getMyElement('#inner-tasks');
 
-      const checkBox = createMyElement('input');
-      checkBox.type = 'checkbox';
-      checkBox.name = 'checkbox';
-      checkBox.className = 'check-box';
-      li.prepend(checkBox);
+        const checkBox = createMyElement('input');
+        checkBox.type = 'checkbox';
+        checkBox.name = 'checkbox';
+        checkBox.className = 'check-box';
+        li.prepend(checkBox);
 
-      const dots = createMyElement('span');
-      dots.innerHTML = '⋮';
-      dots.className = 'dot-line';
-      li.appendChild(dots);
+        const dots = createMyElement('span');
+        dots.innerHTML = '⋮';
+        dots.className = 'dot-line';
+        li.appendChild(dots);
 
-      checkBox.addEventListener('change', (e) => {
-        if (e.target.checked) {
-          data.completed = true;
+        checkBox.addEventListener('change', (e) => {
+          if (e.target.checked) {
+            data.completed = true;
+            li.classList.add('over-line');
+            const key = data.index;
+            localStorage.setItem(key, JSON.stringify(data));
+            li.classList.add('over-line');
+          } else {
+            data.completed = false;
+            li.classList.remove('over-line');
+            const key = data.index;
+            localStorage.setItem(key, JSON.stringify(data));
+            li.classList.remove('over-line');
+          }
+        });
+        if (data.completed === true) {
           li.classList.add('over-line');
-          const key = data.index;
-          localStorage.setItem(key, JSON.stringify(data));
-          li.classList.add('over-line');
+          checkBox.checked = true;
+          ul.prepend(li);
         } else {
-          data.completed = false;
           li.classList.remove('over-line');
-          const key = data.index;
-          localStorage.setItem(key, JSON.stringify(data));
-          li.classList.remove('over-line');
+          checkBox.checked = false;
+          ul.prepend(li);
         }
-      });
-      if (data.completed === true) {
-        li.classList.add('over-line');
-        checkBox.checked = true;
-        ul.prepend(li);
-      } else {
-        li.classList.remove('over-line');
-        checkBox.checked = false;
-        ul.prepend(li);
       }
     }
   });
