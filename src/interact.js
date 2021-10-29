eafcf3755562065e080a4630f57e7264733a66bb
micheaol <micheaol80@gmail.com>
2021-10-29 22:39:30 +0100

    fix: commented code removed

--- src/interact.js
+++ src/interact.js
@@ -5,18 +5,10 @@ const deleteAllTasks = document.querySelector('#clear-all');
 
 deleteAllTasks.addEventListener('click', () => {
   const filterTask = tasks.filter((task) => task.completed !== true);
-  // filterTask.forEach((data, i) => {
-  //   data.index = i + 1;
-
-  // });
   localStorage.setItem('tasks', JSON.stringify(filterTask));
 
   window.location.reload();
 });
 
-// reloadTasks.addEventListener('click', (e) => {
-// //  showToUi()
-
-// });
 
 window.onload = showToUi;
