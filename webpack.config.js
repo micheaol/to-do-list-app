5b8a49d3981ef436c172086b5f7b00682ce3798d
micheaol <micheaol80@gmail.com>
2021-11-01 16:30:46 +0100

    deploy branch

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     main: path.resolve(__dirname, './src/index.js'),
   },
   output: {
-    path: path.resolve(__dirname, ' ./dist'),
+    path: path.resolve(__dirname, './dist'),
     filename: 'index_bundle.js',
   },
   module: {
